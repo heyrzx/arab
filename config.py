0a6b10b4ff9f41c3c1cfa81e692ee5173acf80c9
rizh <92094675+heyrzx@users.noreply.github.com>
2025-02-16 18:07:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ADMINS = int(environ.get("ADMINS", ""))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_NAME = environ.get("DB_NAME", "joinrequet")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
