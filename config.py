a6cedd8812987f2b598b4bfd3f79b3c5d8bfa5e8
rizh <92094675+heyrzx@users.noreply.github.com>
2025-04-06 17:33:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+admin_ids = environ.get("ADMINS", "").split()
+ADMINS = [int(admin_id) for admin_id in admin_ids if admin_id]
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
