71a271c291383a2a44e4ea73d839a359edc49020
rizh <92094675+heyrzx@users.noreply.github.com>
2025-04-06 14:25:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ADMINS = int(environ.get("ADMINS", ""))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "joinrequet")
+DB_NAME = environ.get("DB_NAME", "Cluster1")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
